b6eeb2f626fc1ab1d2e8a957f6cccb3e22f47f56
AnaChiaramonte <anasenai.lopes@gmail.com>
2024-08-21 14:54:20 -0300

    Finalizado os exemplos com list

--- EstruraDeDado/Program.cs
+++ EstruraDeDado/Program.cs
@@ -2,8 +2,54 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Minha primeira branch");
-        Console.WriteLine("Trabalhando com Git\\Github");
-        Console.WriteLine("Mias uma tentativa de trabalhar com Git");
+        //Trabalhando com lista ( LIST )
+        //Criando uma lista
+        List<string> frutas = new List<string>();
+        //adicionar itens na lista
+        frutas.Add("Morango");
+        frutas.Add("Manga");
+        frutas.Add("Uva");
+        frutas.Add("Goiaba");
+        //imprimir os itens da lista
+
+
+        frutas.ForEach(Console.WriteLine);
+
+
+        Console.WriteLine();//Pula linha em branco
+
+        //Imprimir elemento na posição especifica
+        Console.WriteLine("Fruta na indice 2 : " + frutas[2]);
+
+        //Inserir um elmento no indice específico
+        frutas.Insert(1, "Maracuja");
+
+
+        Console.WriteLine();//Pula linha em branco
+
+        Console.WriteLine("Minha lista de frutas : ");
+        frutas.ForEach(Console.WriteLine);
+
+
+
+        //Alterar um elmento no indice especifico
+        frutas[4] = "Pêra";
+        Console.WriteLine("Minha lista de frutas : ");
+        frutas.ForEach(Console.WriteLine);
+
+        //Remove o elemento da lista
+        frutas.RemoveAt(3);
+
+        //Remove o elemento pelo valor do conteudo
+        frutas.Remove("Morango");
+
+        //Imprimir a lista novamente
+        Console.WriteLine();
+        Console.WriteLine("Minha lista de frutas : ");
+        frutas.ForEach(Console.WriteLine);
+        //Apagar todos os elementos da lista
+        frutas.Clear();
+
+
     }
 }
