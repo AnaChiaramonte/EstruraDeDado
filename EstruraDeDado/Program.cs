c99c709ba790935fa8158ef285522e842fbc609f
AnaChiaramonte <anasenai.lopes@gmail.com>
2024-08-21 16:43:18 -0300

    Finalizado o projeto de estrutura de dados

--- EstruraDeDado/Program.cs
+++ EstruraDeDado/Program.cs
@@ -59,7 +59,7 @@
         Dictionary<int, string> carros = new Dictionary<int, string>();
         //Adicionar Dados a um Dicionario
         carros.Add(5, "Corsa");
-        carros.Add(10, "Fusca");
+        carros.Add(10, "Fusca");//diferente da lista que o indice é automatico
         carros.Add(2, "Ford ka");
 
         Console.WriteLine();
@@ -70,8 +70,82 @@
         foreach (var carro in carros)
         {
             Console.WriteLine($"{carro.Key} - {carro.Value}");
-
-
         }
+        Console.WriteLine();
+        Console.WriteLine("=====================================");
+        Console.WriteLine();
+
+        //TRABALHANDO COM FILA
+
+        //criar uma lista (Queue)-fila
+        Queue<string> filaBanco = new Queue<string>();
+
+        //Adicionar Elementos em uma Fila
+        filaBanco.Enqueue("André");
+        filaBanco.Enqueue("João");
+        filaBanco.Enqueue("Mariana");
+        filaBanco.Enqueue("Bia");
+
+        foreach (var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+
+        filaBanco.Dequeue();
+        Console.WriteLine();
+        Console.WriteLine("=======================================================");
+        Console.WriteLine();
+
+        foreach (var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+
+        bool achou = filaBanco.Contains("Bia");
+
+        if (achou == true)
+        {
+            Console.WriteLine("A Pessoa esta na fila");
+        }
+        else
+        {
+            Console.WriteLine("A pessoa não esta na fila");
+        }
+        Console.WriteLine();
+        Console.WriteLine("=======================================================");
+        Console.WriteLine();
+
+        //TRABALHANDO COM PILHA (STRACK)
+        //Criando uma Pilha
+        Stack<string> livros = new Stack<string>();
+
+        //Adicionar elementos em uma pilha
+
+        livros.Push("Chapeuzinho Vermelho");
+        livros.Push("Branca de neve e os sete anões");
+        livros.Push("Princesa e o sapo");
+
+        foreach (var livro in livros)
+        {
+
+            Console.WriteLine(livro);
+        }
+        Console.WriteLine();
+        Console.WriteLine("=======================================================");
+        Console.WriteLine();
+
+        //Remove o primeiro elemento da Pilha
+        livros.Pop();
+        foreach (var livro in livros)
+        {
+            Console.WriteLine(livro);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("=======================================================");
+        Console.WriteLine();
+
+
+
     }
 }
