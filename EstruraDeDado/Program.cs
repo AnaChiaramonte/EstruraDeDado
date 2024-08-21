39cad70ba8590edae09c92d66f13822361e26b95
AnaChiaramonte <anasenai.lopes@gmail.com>
2024-08-21 15:24:51 -0300

    Dicionario de carros

--- EstruraDeDado/Program.cs
+++ EstruraDeDado/Program.cs
@@ -8,8 +8,8 @@
         //adicionar itens na lista
         frutas.Add("Morango");
         frutas.Add("Manga");
-        frutas.Add("Uva");
         frutas.Add("Goiaba");
+        frutas.Add("Uva");
         //imprimir os itens da lista
 
 
@@ -30,12 +30,14 @@
         Console.WriteLine("Minha lista de frutas : ");
         frutas.ForEach(Console.WriteLine);
 
-
+        Console.WriteLine();//Pula linha em branco
 
         //Alterar um elmento no indice especifico
         frutas[4] = "Pêra";
         Console.WriteLine("Minha lista de frutas : ");
         frutas.ForEach(Console.WriteLine);
+
+        Console.WriteLine();//Pula linha em branco
 
         //Remove o elemento da lista
         frutas.RemoveAt(3);
@@ -50,6 +52,26 @@
         //Apagar todos os elementos da lista
         frutas.Clear();
 
+        //TRABALHANDO COM DICIONARIOS DE DADOS
 
+        //Criando um dicionario de dados
+
+        Dictionary<int, string> carros = new Dictionary<int, string>();
+        //Adicionar Dados a um Dicionario
+        carros.Add(5, "Corsa");
+        carros.Add(10, "Fusca");
+        carros.Add(2, "Ford ka");
+
+        Console.WriteLine();
+        Console.WriteLine("=====================================");
+        Console.WriteLine();
+        Console.WriteLine("Meu Dicionario de carros : ");
+        //Imprimir um dicionario de dados
+        foreach (var carro in carros)
+        {
+            Console.WriteLine($"{carro.Key} - {carro.Value}");
+
+
+        }
     }
 }
